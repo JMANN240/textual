ea4266e17c5ae366cec8910f7cb3a8161134e4bf
JT Raber <JMANN240raber@gmail.com>
2023-04-01 18:30:36 -0400

    added some comments and made a pretty layout

--- create_world.py
+++ create_world.py
@@ -2,10 +2,29 @@ from room import Room, Door
 from player import Player
 from item import Key
 
+# The example layout
+# x: locked door
+# o: unlocked door
+# 'u': player
+# F: key
+
+#
+#       outside
+#
+#     +--x--+
+#     |     |
+# +---+     |
+# | F o 'u' |
+# +---+-----+
+#
+
+# Rooms
 starting_room = Room("starting room")
 closet = Room("closet")
 outside = Room("outside")
 
+
+# Doors
 starting_room_closet_door = Door(
     starting_room, "to the left",
     closet, "in front of you",
@@ -18,6 +37,9 @@ front_door = Door(
     locked=True
 )
 
+
+# Items
 key = Key("front door key", closet, front_door)
 
+# The player
 player = Player("JT", starting_room)
